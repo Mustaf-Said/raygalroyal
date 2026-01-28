31b0f57783db7566d223567bcdfd2d2448452d4f
Mustafa <86mussai@skola.boras.se>
2026-01-28 19:39:08 +0100

    DelEn

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html suppressHydrationWarning>
       <body className="abril-fatface-regular">
 
         {/* Weglot initialization component */}
