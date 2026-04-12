f5a6cd44f41146faa61cf96a5d3ea80161918da5
Mustafa <86mussai@skola.boras.se>
2026-04-12 21:00:29 +0200

    FixedLOgo

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/opengraph-image",
+        url: "/logoB.png",
         width: 1200,
         height: 630,
         alt: "Raygal Royal",
