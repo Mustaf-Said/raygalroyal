0c690501f03e8b0d86495e961ffa11afaa4ed9e2
Mustafa <86mussai@skola.boras.se>
2026-05-25 10:48:02 +0200

    FixedCssErrors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import "../styles/globals.css";
+// @ts-expect-error CSS imports are handled by Next.js
+import "../styles/globals.css"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { LanguageProvider } from "./components/LanguageProvider";
